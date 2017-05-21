88b5c025a66cdb6f22e579cf7a1df77f259a3404
JCYTopWorld <jcyemail@qq.com>
2017-05-21 16:12:45 +0800

    10
    检测鼠标点击到了哪个Cube

--- TowerDefense/Assets/Scenes/MainScene/Scripts/BuildManager.cs
+++ TowerDefense/Assets/Scenes/MainScene/Scripts/BuildManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BuildManager : MonoBehaviour
 {
@@ -8,6 +9,24 @@ public class BuildManager : MonoBehaviour
     public TurretData missileTurretData;
     public TurretData standarTurretData;
     public TurretData selectedTurretData;//表示将要建造的炮台
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (EventSystem.current.IsPointerOverGameObject() == false)
+            {
+                //进行开发炮台的建造
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+               bool isCollider= Physics.Raycast(ray,out hit , 1000, LayerMask.GetMask("MapCube"));
+                if (isCollider)
+                {
+                    GameObject mapCube = hit.transform.gameObject;
+                }
+            }
+        }
+    }
 
     public void OnLaserSelected(bool isOn)
     {
