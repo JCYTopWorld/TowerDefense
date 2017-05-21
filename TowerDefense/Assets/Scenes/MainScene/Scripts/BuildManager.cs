0586c142b10afdce4efd15c771f294917230f656
JCYTopWorld <jcyemail@qq.com>
2017-05-21 15:57:54 +0800

    09
    监听炮塔选择的事件

---
+++ TowerDefense/Assets/Scenes/MainScene/Scripts/BuildManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public TurretData laserTurretData;
+    public TurretData missileTurretData;
+    public TurretData standarTurretData;
+    public TurretData selectedTurretData;//表示将要建造的炮台
+
+    public void OnLaserSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = laserTurretData;
+        }
+    }
+
+    public void OnMissileSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = missileTurretData;
+        }
+    }
+
+    public void OnStandarSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            selectedTurretData = standarTurretData;
+        }
+    }
+}
