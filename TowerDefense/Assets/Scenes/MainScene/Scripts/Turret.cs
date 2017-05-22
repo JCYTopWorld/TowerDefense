a23ba1c94df90302eedcbbbc4a5232aa6c4ea415
JCYTopWorld <jcyemail@qq.com>
2017-05-22 11:43:46 +0800

    16
    添加触发器，检测炮台附近的敌人

---
+++ TowerDefense/Assets/Scenes/MainScene/Scripts/Turret.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public List<GameObject> enemys = new List<GameObject>();
+     
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag=="Enemy")
+        {
+            enemys .Add(collider .gameObject );
+        }
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.tag=="Enemy")
+        {
+            enemys.Remove(collider.gameObject);
+        }
+    }
+}
