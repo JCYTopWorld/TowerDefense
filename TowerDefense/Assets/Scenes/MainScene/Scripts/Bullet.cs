1c37d3e07ce1e7ef599c9cf8066124a4041b0cb9
JCYTopWorld <jcyemail@qq.com>
2017-05-22 14:50:49 +0800

    20
    增加爆炸特效检测修改

--- TowerDefense/Assets/Scenes/MainScene/Scripts/Bullet.cs
+++ TowerDefense/Assets/Scenes/MainScene/Scripts/Bullet.cs
@@ -8,7 +8,7 @@ public class Bullet : MonoBehaviour
     public float speed = 100;
     private Transform target;
     public GameObject explosionEffectPrefab;
-
+    private float distanceArriveTarget = 1.2f;
     public void SetTarget(Transform _target)
     {
         this.target = _target;
@@ -18,14 +18,12 @@ public class Bullet : MonoBehaviour
     {
         transform.LookAt(target.position);
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
-    }
-
-    void OnTriggerEnter(Collider collider)
-    {
-        if (collider.tag == "Enemy")
+        Vector3 dir = target.position - transform.position;
+        if (dir.magnitude < distanceArriveTarget)
         {
-            collider.GetComponent<Enemy>().TakeDamage(damage);
-            GameObject.Instantiate(explosionEffectPrefab, transform.position, transform.rotation);
+            target.GetComponent<Enemy>().TakeDamage(damage);
+            GameObject effect = GameObject.Instantiate(explosionEffectPrefab, transform.position, transform.rotation);
+            Destroy(effect, 1f);
             Destroy(this.gameObject);
         }
     }
