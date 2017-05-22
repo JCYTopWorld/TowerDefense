0581c532f8f0c640dc3741e310b89a6aaca53a78
JCYTopWorld <jcyemail@qq.com>
2017-05-22 11:08:58 +0800

    15
    利用粒子系统创建建造的特效

--- TowerDefense/Assets/Scenes/MainScene/Scripts/MapCube.cs
+++ TowerDefense/Assets/Scenes/MainScene/Scripts/MapCube.cs
@@ -6,9 +6,12 @@ public class MapCube : MonoBehaviour
 {
     [HideInInspector]
     public GameObject turretGo;
+    public GameObject buildEffect;
 
     public void BuildTurret(GameObject turretGameObject)
     {
         turretGo = GameObject.Instantiate(turretGameObject, transform.position, Quaternion.identity);
+        GameObject effect = GameObject.Instantiate(buildEffect, transform.position, Quaternion.identity);
+        Destroy(effect, 0.2f);
     }
 }
